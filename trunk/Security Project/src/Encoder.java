58cb6b648594a94cf5644dd0619fcf364e193955
michael.kamal@gmail.com <michael.kamal@gmail.com@0e1012e7-7d05-273e-ae8f-e955a312098c>
2012-05-24 15:34:52 +0000

    encoder final

--- trunk/Security Project/src/Encoder.java
+++ trunk/Security Project/src/Encoder.java
@@ -5,15 +5,18 @@ import java.io.InputStreamReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class Encoder {
 
 	
 	BufferedReader userInput;
+	boolean correct;
 	
 	public Encoder()
 	{
+		correct = false;
 		userInput = new BufferedReader (new InputStreamReader(System.in));
 		
 	}
@@ -24,7 +27,19 @@ public class Encoder {
 		try
 		{
 			System.out.println("Enter a password:");
-	        password = userInput.readLine();	
+			password = userInput.readLine();
+			
+			if(!password.contains(" "))
+				correct = true;
+			
+			while(!correct){
+				System.out.println("Password cannot contain spaces, Enter a password:");
+				password = userInput.readLine();
+				
+				if(!password.contains(" "))
+					correct = true;
+	        }
+			
 	        userInput.close();
 	        
 	        return password;
@@ -45,13 +60,49 @@ public class Encoder {
 	
 	public void encrypt(String password){
 	
+		String hashPassword = "";
 		String salt = generateSalt();
+		String hashTempString = password + salt;
+		int stringLength = hashTempString.length();
+		char [] hash = new char[password.length()+salt.length()];
+		//char [] hash = new char[password.length()+salt.length()];
+		ArrayList<String> hashTemp = new ArrayList<String>();
+		
+		for(int i = 0; i < stringLength; i++){
+			hash[i]=hashTempString.charAt(i);
+		}
+		
+		hashTemp.add(hashTempString.substring(0, stringLength/4));
+		hashTemp.add(hashTempString.substring(stringLength/4, stringLength/2));
+		hashTemp.add(hashTempString.substring(stringLength/2, (stringLength*3)/4));
+		hashTemp.add(hashTempString.substring((stringLength*3)/4, stringLength));
+		
+		hashTempString = hashTemp.get(0) + hashTemp.get(2) + hashTemp.get(3) + hashTemp.get(1); 
+		
+		hashTemp = new ArrayList<String>();
+		
+		hashTemp.add(hashTempString.substring(0, stringLength/6));
+		hashTemp.add(hashTempString.substring(stringLength/6, (2*stringLength)/6));
+		hashTemp.add(hashTempString.substring((2*stringLength)/6, stringLength/2));
+		hashTemp.add(hashTempString.substring(stringLength/2, (2*stringLength)/3));
+		hashTemp.add(hashTempString.substring((2*stringLength)/3, (5*stringLength)/6));
+		hashTemp.add(hashTempString.substring((5*stringLength)/6, stringLength));
+		
+		hashTempString = hashTemp.get(3) + hashTemp.get(5) + hashTemp.get(1) + hashTemp.get(4) + hashTemp.get(0) + hashTemp.get(2);
+		hashTemp = new ArrayList<String>();
+		
+		hashTemp.add(hashTempString.substring(0, stringLength/2));
+		hashTemp.add(hashTempString.substring(stringLength/2, stringLength));
+		hashPassword = hashTemp.get(1) + hashTemp.get(0);
+		
+		writeInPasswordFile(hashPassword, salt);
+		
 	}
 		
 	public static void writeInPasswordFile(String hashedPassword, String salt){
 		String outFile = "/Users/michaelmkamal/Documents/workspace/Security Project/passwordFile.out";
 		try {
-			BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
+			BufferedWriter writer = new BufferedWriter(new FileWriter(outFile,true));
 			writer.write(hashedPassword + ":" + salt);
 			writer.newLine();
 			writer.close();
@@ -65,7 +116,7 @@ public class Encoder {
 		
 		encrypt(getPassword());
 		
-		writeInPasswordFile("Apple", "GUC");
+		//writeInPasswordFile("Apple", "GUC");
 
 	}
 }
