0fdb3340087922991dcf2d79fb1b23b1048fd607
michael.kamal@gmail.com <michael.kamal@gmail.com@0e1012e7-7d05-273e-ae8f-e955a312098c>
2012-05-24 13:06:28 +0000

    writing to password file

--- trunk/Security Project/src/Encoder.java
+++ trunk/Security Project/src/Encoder.java
@@ -1,3 +1,7 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 
 
 public class Encoder {
@@ -10,8 +14,20 @@ public class Encoder {
 		return salt;
 	}
 	
+	public static void writeInPasswordFile(String hashedPassword, String salt){
+		String outFile = "/Users/michaelmkamal/Documents/workspace/Security Project/src/passwordFile.out";
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
+			writer.write(hashedPassword + ":" + salt);
+			writer.newLine();
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public static void main(String[]args){
 		//System.out.println(generateSalt());
-		
+		writeInPasswordFile("Apple", "GUC");
 	}
 }
